7f640068fc419ce15f6e539c11f07c6734dc8afe
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-06-12 14:35:11 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
 import { useState, useEffect } from "react";
-import { ChevronLeft,ChevronRight  } from 'lucide-react';
+import { ChevronLeft, ChevronRight, Info, Earth } from 'lucide-react';
+import { createPortal } from "react-dom";
+import Map from "./components/Map";
+import InfosCountries from '../src/components/InfosCountries'
 import './App.css';
 
 export default function App() {
   const [countries, setCountries] = useState([]);
   const [index, setIndex] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
+  const [showPortal, setShowPortal] = useState(false);
 
   useEffect(() => {
-    fetch("https://restcountries.com/v3.1/all ")
+    fetch("https://restcountries.com/v3.1/all")
       .then(response => response.json())
       .then(response => {
-        console.log(response);
         setCountries(response);
         setIsLoading(false);
       })
       .catch(error => {
-        console.log('une erreur est subitement arrivée', error);
+        console.error('An error occurred:', error);
         setIsLoading(false);
       });
   }, []);
@@ -31,28 +34,35 @@ export default function App() {
 
   return (
     <div className="bg-zinc-900 w-full h-screen flex justify-center items-center">
-      <div className="bg-red-500 h-30 w-full sm:w-[450px]  rounded-xl shadow-xl">
+      <div className="bg-red-500 h-30 w-full sm:w-[450px] rounded-xl shadow-xl">
         <div className="flex justify-center w-full">
           <h2 className="text-2xl py-2 font-semibold text-gray-300">Flags App</h2>
         </div>
         <div className="h-auto py-5 rounded-sm flex justify-center flex-col items-center">
           <div className="w-1/2">
-            {isLoading ? (<p>Chargement ...</p>) : (<img className="w-30 rounded-t-xl py-2 h-30" src={countries[index].flags.svg} alt="" />)}
+            {isLoading ? (<p>Loading ...</p>) : (<img className="w-30 rounded-t-xl py-2 h-30" src={countries[index].flags.svg} alt={countries[index].name.common} />)}
           </div>
           <div className="flex items-center justify-center py-2 bg-slate-200 w-1/2 rounded-b-xl h-auto">
-            {isLoading ? (<p>Chargement ...</p>) : (<p className="text-zinc-900  text-xl  truncate font-karla">{countries[index].name.common}</p>)}
+            {isLoading ? (<p>Loading ...</p>) : (<p className="text-zinc-900 text-xl truncate font-karla">{countries[index].name.common}</p>)}
           </div>
         </div>
-        <div className="w-full bg-gray-200 rounded-b-md py-2 flex   justify-center">
-
-          <button  className="bg-red-500 rounded-xl py-2 px-2 shadow-sm mx-1 active:bg-red-800" onClick={()=> previousCountries()}>
-          <ChevronLeft color="#f1f1f1" />
+        <div className="w-full bg-gray-200 rounded-b-md py-2 flex justify-center">
+          <button className="bg-red-500 rounded-xl py-2 px-2 shadow-sm mx-1 active:bg-red-800" onClick={previousCountries}>
+            <ChevronLeft color="#f1f1f1" />
           </button>
-          <button className="bg-red-500 rounded-xl py-2 px-2 shadow-sm  active:bg-red-800 mx-1" onClick={() => nextCountries()}>
-          <ChevronRight color="#f1f1f1" />
+          <button className="bg-red-500 rounded-xl py-2 px-2 shadow-sm mx-1" onClick={() => setShowPortal(true)}>
+            <Info color="#fafafa" />
+          </button>
+          <button className="bg-red-500 rounded-xl py-2 px-2 shadow-sm mx-1" onClick={() => setShowPortal(true)}>
+            <Earth color="#fafafa" />
+          </button>
+          <button className="bg-red-500 rounded-xl py-2 px-2 shadow-sm mx-1" onClick={nextCountries}>
+            <ChevronRight color="#f1f1f1" />
           </button>
         </div>
       </div>
+      {showPortal && createPortal(<Map close={() => setShowPortal(false)}/>, document.body)}
+      {showPortal && createPortal(<InfosCountries close={() => setShowPortal(false)}/>, document.body)}
     </div>
   );
 }
---
+++ src/components/Map.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+export default function Map({ close, map }) {
+    return (
+        <div onClick={close}
+            className="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center">
+            <div className='bg-gray-300 w-1/2'>
+    
+            </div>
+        </div>
+    );
+}
